7c68c344ac15526ba78aeaf90e804e28f6702ac3
Dedy Surya Wijaya <dedysurya2000@gmail.com>
2026-05-23 11:21:49 +0800

    change port

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import { web } from "./application/web.js";
 import { logger } from "./application/logging.js";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 web.listen(PORT, () => {
     logger.info(`Server is running on http://localhost:${PORT}`);
 });
